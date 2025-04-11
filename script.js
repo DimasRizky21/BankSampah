97ca4356ed91a2f20b1893eec8b9e2f0bf27e1ce
Syahrul Setiawan <syahrulsetiawan2801@gmail.com>
2025-04-11 09:29:46 +0700

    reload button

---
+++ script.js
@@ -0,0 +1,11 @@
+// Ambil parameter dari URL
+const urlParams = new URLSearchParams(window.location.search);
+const fromHome = urlParams.get("from");
+
+// Cek jika user datang bukan dari home ATAU halaman di-refresh
+if (
+  fromHome !== "home" ||
+  performance.getEntriesByType("navigation")[0].type === "reload"
+) {
+  window.location.href = "index.html";
+}
